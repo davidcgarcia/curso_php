ba5a68b83cb82d865cc86490e7651d4b5711002f
davidcgarcia <davidg9404@gmail.com>
2016-06-30 14:03:51 -0500

    Arreglos asociativos!

--- arreglos.php
+++ arreglos.php
@@ -1,14 +1,32 @@
 <?php
 
-# Declara una variable de tipo array => arreglo
+# Los arreglos son variables que nos permiten almacenar múltiples valores.
+
+# Ejemplo de un arreglo con los días de la semana.
 $semana = array('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo');
 
-# Le agrega un nuevo valor al arreglo
-$semana[7] = 'Juernes';
-echo $semana[7];
+# En los arreglos podemos almacenar cualquier tipo de dato, incluso podemos almacenar un arreglo dentro de otro
 
-# Declara una variable de tipo array => arreglo
-$semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+$arreglo = array('cadenas de texto', 1, array('asd', 123), true);
+
+# Ejemplo en el que mostramos los días de la semana en pantalla a partir de nuestro arreglo llamado semana
+
+echo $semana[0] . '<br>';
+echo $semana[1] . '<br>';
+echo $semana[2] . '<br>';
+echo $semana[3] . '<br>';
+echo $semana[4] . '<br>';
+echo $semana[5] . '<br>';
+echo $semana[6] . '<br>';
+
+
+# tipos de arreglos
+
+# 1. Arreglo indexado - Como el que vimos en el ejemplo anterior
+
+# 2. Arreglo asociativo: En el cual podemos acceder a los valores del arreglo mediante cadenas de texto que nosotros especifiquemos. Por ejemplo: 
+
+# 3. Arreglos Multidimensionales: Son aquellos que contienen un arreglo dentro de otro arreglo
 
 
 ?>
---
+++ arreglos_asociativos.php
@@ -0,0 +1,10 @@
+<?php
+
+  # Arreglos asociativos
+  $david = array('telefono' => '45778', 'edad' => 25, 'apellido' => 'Garcia', 'pais' => 'Colombia');
+
+  echo $david['telefono'] . '<br>';
+  echo $david['edad'] . '<br>';
+  echo $david['apellido'] . '<br>';
+  echo $david['pais'] . '<br>';
+?>
