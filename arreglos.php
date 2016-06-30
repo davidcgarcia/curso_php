0ba7076388fcfd96f3765590e4d3a91b263ef80d
davidcgarcia <davidg9404@gmail.com>
2016-06-30 13:50:36 -0500

    Arreglos!

---
+++ arreglos.php
@@ -0,0 +1,14 @@
+<?php
+
+# Declara una variable de tipo array => arreglo
+$semana = array('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo');
+
+# Le agrega un nuevo valor al arreglo
+$semana[7] = 'Juernes';
+echo $semana[7];
+
+# Declara una variable de tipo array => arreglo
+$semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+
+?>
