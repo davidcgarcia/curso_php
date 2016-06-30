0c96577184c64f93977ef115e270580eebe87257
davidcgarcia <davidg9404@gmail.com>
2016-06-30 14:38:31 -0500

    Recorrer arreglos!

---
+++ recorrer_arreglos.php
@@ -0,0 +1,37 @@
+<?php
+
+$meses = [
+    'Enero', 
+    'Febrero', 
+    'Marzo', 
+    'Abril', 
+    'Mayo', 
+    'Junio', 
+    'Julio', 
+    'Agosto', 
+    'Septiembre', 
+    'Octubre', 
+    'Noviembre', 
+    'Diciembre'
+  ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Meses del año</title>
+</head>
+<body>
+  <h1>Meses del año</h1>
+
+  <ul>
+    <?php 
+      foreach ($meses as $mes) {
+        echo '<li>' . $mes . '</li>';
+      }
+    ?>
+  </ul>
+</body>
+</html>
