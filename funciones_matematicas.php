2d9d30dfc1487f841801e5ec662ffa799871cb33
davidcgarcia <davidg9404@gmail.com>
2016-07-01 15:28:32 -0500

    Funciones matematicas

---
+++ funciones_matematicas.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero = 15.485;
+
+// redondea un numero al valor decimal más cernano
+// echo round($numero, 3);
+
+// Permite obtener un numero al azar entre dos numeros dados
+// echo rand(1, 100);
+
+// Permite redondear un número hacía arriba
+
+// echo ceil($numero);
+
+// CONSTANTE: Número PI incluido por PHP
+echo M_PI;
+
+?>
