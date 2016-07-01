23b67e8b784b9efb0d199597c9f2b23b716ef8eb
davidcgarcia <davidg9404@gmail.com>
2016-07-01 13:58:52 -0500

    Funcion print_r

---
+++ print_r.php
@@ -0,0 +1,23 @@
+<?php
+
+$texto = 'Cristhian';
+$numero = 10;
+$numero2 = '5';
+$arreglo = [
+  'Cristhian', 
+  'Cesar', 
+  'Alejandro'
+];
+
+$arregloAsociativo = [
+  'nombre'  => 'Cristhian', 
+  'edad'    => 20
+];
+
+$booleano = true;
+
+echo '<pre>';
+print_r($arregloAsociativo);
+echo '</pre>';
+
+?>
