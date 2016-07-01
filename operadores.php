13bc2bbd7c3ed5de7cf0e8471bd6538314724d3c
davidcgarcia <davidg9404@gmail.com>
2016-07-01 07:17:32 -0500

    Operadores

---
+++ operadores.php
@@ -0,0 +1,100 @@
+<?php
+  
+/** 
+* Operadores Aritméticos: 
+*
+* + Suma
+* - Resta
+* * Multiplicación
+* / División
+* % Modulo
+*
+*/
+
+// $numero = 10;
+// $numero2 = 5;
+
+// $resultado = $numero + $numero2;
+
+// echo $resultado . '<br>';
+
+/** 
+* Operadores de asignación:
+*
+* =
+* += 
+* -= 
+* /= 
+*
+*/
+
+// $numero = 10;
+// $numero2 = 5;
+
+// $numero *= 7;
+// echo $numero;
+
+/** 
+* Operadores de Comparación: 
+*
+* ==
+* ===
+* !=, <> 
+* !==
+* >
+* <
+* >= 
+* <=
+*
+*/
+
+/*$numero = 10;
+$variable = true;
+
+if ($numero === 10) {
+  echo 'Es identico a ' . $numero;
+} else {
+  echo 'Es diferente';
+}*/
+
+/** 
+* Operadores lógicos: 
+* 
+* and, && 
+* or, || 
+* xor
+* !
+*
+*/
+
+/*$numero = 10;
+if ($numero >= 10 && $numero < 20) {
+  echo 'ejecuta';
+}*/
+
+/** 
+* Operadores de incremento
+*
+* ++$x
+* $x++
+* --$x
+* $x--
+*
+*/
+
+// $numero = 10;
+// echo ++$numero;
+
+/** 
+* Operadores de Cadenas: 
+* 
+* .
+* .= 
+*
+*/
+
+$texto = 'Cadena de Texto ';
+$texto .= 'Una segunda cadena de Texto';
+
+echo $texto;
+?>
