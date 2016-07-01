b8b07f48b94f45cec730893767148c8a3f9ebdca
davidcgarcia <davidg9404@gmail.com>
2016-07-01 14:45:48 -0500

    Funciones utiles para cadenas de texto

---
+++ funciones_cadenas_de_texto.php
@@ -0,0 +1,30 @@
+<?php
+
+$texto = 'Hola';
+
+// Convierte caracteres especiales en entidades HTML
+// echo htmlspecialchars($texto);
+
+// Elimina espacios en blanco (u otro tipo de caracteres) del inicio y el final de la cadena
+//echo trim($texto);
+
+// Calcula el tamaño o la longitud de una cadena de texto;
+
+//echo strlen($texto);
+
+// Devuelve parte de una cadena de texto
+
+// echo substr($texto, 0, 4);
+
+// Covierte todos los caracteres de la cadena en mayúscula
+
+// echo strtoupper($texto);
+
+// Convierte todos los caracteres de la cadena a minúscula
+//echo strtolower($texto);
+
+// permite conocer en que posición se encuentra una letra
+
+echo strpos($texto, 'a');
+
+?>
