322ebf848d09ab22c4069bfc79f5031600964679
davidcgarcia <davidg9404@gmail.com>
2016-07-02 17:05:50 -0500

    Validando datos del formulario con PHP

--- practicas/formulario_contacto/index.view.php
+++ practicas/formulario_contacto/index.view.php
@@ -10,21 +10,27 @@
   
   <div class="wrap">
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+      <h2>Contactame</h2>
       <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre" value="">
 
       <input type="text" name="correo" id="correo" class="form-control" placeholder="Correo" value="">
 
       <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje"></textarea>
       
-      <!-- <div class="alert error">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      </div>
 
-      <div class="alert success">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      </div> -->
+      <?php if (!empty($errores)): ?>
+        <div class="alert error">
+          <?php echo $errores; ?>
+        </div>
 
-      <input type="submit" class="btn btn-primary" value="Enviar Correo">
+      <?php elseif($enviado): ?>
+        <div class="alert success">
+          <p>Enviado correctamente!</p>
+        </div>
+      <?php endif; ?>
+
+
+      <input type="submit" class="btn btn-primary" name="submit" value="Enviar Correo">
     </form>
   </div>
 
