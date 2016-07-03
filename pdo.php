acfb76bafdcf6f1544f0479983fcfa8b547dc27f
davidcgarcia <davidg9404@gmail.com>
2016-07-03 16:39:21 -0500

    Consultas preparadas (Prepared Statements)

--- pdo.php
+++ pdo.php
@@ -1,15 +1,33 @@
 <?php
 
+$id = $_GET['id'];
+
 try {
   $conexion = new PDO('mysql:host=localhost;dbname=prueba_consola', 'root', 'L4r4v3l*.*2016');
   
+  /** 
+  * Ejecuantando consultas mediante el método Query
   $sql = "select * from usuarios";
 
   $resultados = $conexion->query($sql);
 
   foreach ($resultados as $fila) {
     echo $fila['nombre'] . '<br>';
-  }
+  }*/
+
+  // Consultas Preparadas - Prepared Statements
+
+  // $statement = $conexion->prepare('SELECT * FROM usuarios WHERE id = :id');
+  $statement = $conexion->prepare('INSERT INTO usuarios(id, nombre, email) VALUES(null, "Natalia Mutiz", "nmutiz@gmail.com")');
+
+  // $statement->execute([':id' => $id]);
+  $statement->execute();
+
+ /* $rows = $statement->fetchAll();
+  
+  foreach ($rows as $row) {
+    echo $row['nombre'] . '<br>';
+  }*/
 
 } catch (PDOException $e) {
   echo 'Error: ' . $e->getMessage();
