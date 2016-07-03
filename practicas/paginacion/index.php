133bab81ee1a2263497faef54c6d65069be3907d
davidcgarcia <davidg9404@gmail.com>
2016-07-03 18:56:46 -0500

    Creando la logina de la paginacion (Parte 2)

--- practicas/paginacion/index.php
+++ practicas/paginacion/index.php
@@ -13,6 +13,24 @@
   $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
   $postPorPagina = 5;
 
-    
+  $inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
+
+  $sql = 'SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT ' . $inicio . ',' . $postPorPagina;
+  $articulos = $conexion->prepare($sql);
+  $articulos->execute();
+  $articulos = $articulos->fetchAll();
+
+  if (!$articulos) {
+    header('Location: index.php');
+  }
+
+  $totalArticulos = $conexion->query('SELECT FOUND_ROWS() as total');
+  $totalArticulos = $totalArticulos->fetch()['total'];
+
+  $numeroPaginas = ceil($totalArticulos / $postPorPagina);
+
+  echo $numeroPaginas;
+
+
   require 'index.view.php';
 ?>
