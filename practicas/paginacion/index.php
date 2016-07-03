778d61d1f0f802fb9ef6918c6b77cf34a26f821c
davidcgarcia <davidg9404@gmail.com>
2016-07-03 18:39:54 -0500

    Creando la logica de la paginacion

--- practicas/paginacion/index.php
+++ practicas/paginacion/index.php
@@ -1,4 +1,18 @@
 <?php
 
+  try {
+
+    $conexion = new PDO('mysql:host=localhost;dbname=paginacion', 'root', 'L4r4v3l*.*2016');
+
+  } catch (PDOException $e) {
+    echo 'Error DB: ' . $e->getMessage();
+    die();
+  }
+
+
+  $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+  $postPorPagina = 5;
+
+    
   require 'index.view.php';
 ?>
