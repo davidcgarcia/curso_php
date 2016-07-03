ab7fd9f921ab421b676d94f3060ae98b197bb630
davidcgarcia <davidg9404@gmail.com>
2016-07-03 17:13:58 -0500

    Creando la estructura HTML y CSS de la paginacion

---
+++ practicas/paginacion/index.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Paginación</title>
+  <link href='https://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+  
+  <div class="container">
+    <h1>Articulos</h1>
+
+    <section class="articles">
+      <ul>
+        <li>1.- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque</li>
+        <li>2.- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque</li>
+        <li>3.- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque</li>
+        <li>4.- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque</li>
+        <li>5.- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque</li>
+      </ul>
+    </section>
+
+    <section class="paginate">
+      <ul>
+        <li class="disable">&laquo;</li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li><a href="#">&raquo;</a></li>
+      </ul>
+    </section>
+  </div>
+
+</body>
+</html>
