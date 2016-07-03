35f7e27365bca294b29a132fd8a5a93aba56534c
davidcgarcia <davidg9404@gmail.com>
2016-07-03 16:24:29 -0500

    Ejecutando consultas mediante el metodo query

--- pdo.php
+++ pdo.php
@@ -1,8 +1,16 @@
 <?php
 
 try {
-  $conexion = new PDO('mysql:host = localhost; dbname = prueba_consola', 'root', 'L4r4v3l*.*2016');
-  echo 'Conexion OK';
+  $conexion = new PDO('mysql:host=localhost;dbname=prueba_consola', 'root', 'L4r4v3l*.*2016');
+  
+  $sql = "select * from usuarios";
+
+  $resultados = $conexion->query($sql);
+
+  foreach ($resultados as $fila) {
+    echo $fila['nombre'] . '<br>';
+  }
+
 } catch (PDOException $e) {
   echo 'Error: ' . $e->getMessage();
 }
