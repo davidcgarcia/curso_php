bf930dbbdfab4df8aefe914d03ae8f76bbb7b657
davidcgarcia <davidg9404@gmail.com>
2016-07-03 20:16:05 -0500

    Creando la vista del formulario del login

--- practicas/login_registro/views/login.view.php
+++ practicas/login_registro/views/login.view.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <title>Registrate</title>
+  <title>Iniciar Sesión</title>
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,300italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
   <div class="contenedor">
-    <h1 class="titulo">Registrate</h1>
+    <h1 class="titulo">Iniciar Sesión</h1>
     <hr class="border">
 
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" class="formulario" name="login">
@@ -19,18 +19,14 @@
       </div>
 
       <div class="form-group">
-        <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña">
-      </div>
-
-      <div class="form-group">
-        <i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir contraseña">
+        <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña">
         <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
       </div>
     </form>
     
     <p class="texto-registrate">
-      ¿ Ya tienes cuenta ?
-      <a href="login.php">Iniciar Sesión</a>
+      ¿ Aún tienes cuenta ?
+      <a href="registro.php">Registrate</a>
     </p>
   </div>
 </body>
