bbc2870b9de3d46afa377cd02a06c8a918d64398
davidcgarcia <davidg9404@gmail.com>
2016-07-03 19:50:56 -0500

    Creando el index y preparando la pagina de registro

---
+++ practicas/login_registro/registro.php
@@ -0,0 +1,4 @@
+<?php
+
+  require 'views/registro.view.php';
+?>
--- practicas/login_registro/views/contenido.view.php
+++ practicas/login_registro/views/contenido.view.php
@@ -5,13 +5,14 @@
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <title>Login Registro</title>
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,300italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
   <div class="contenedor">
     <h1 class="titulo">Contenido del sitio</h1>
     <a href="cerrar.php">Cerrar Sesion</a>
-    
+    <hr class="border">
     <div class="contenido">
       <article>
         <p>
@@ -20,6 +21,10 @@
 
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque optio quidem architecto dignissimos fugit, eius autem vitae deserunt, consequuntur nihil amet minus, distinctio voluptate fugiat ipsam similique, saepe quibusdam. Veniam.
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, ipsum. Temporibus assumenda cumque tempora perferendis et itaque dignissimos ab facere porro ducimus neque ipsa, dicta nemo aperiam praesentium labore delectus!
         </p>
       </article>
     </div>
