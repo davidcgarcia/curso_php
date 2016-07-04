ca12c0edc23df63ee8c52b017e6f5af4b0dd076a
davidcgarcia <davidg9404@gmail.com>
2016-07-03 19:43:07 -0500

    Creando la estructura base y contenido a proteger

---
+++ practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <title>Login Registro</title>
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,300italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+  <div class="contenedor">
+    <h1 class="titulo">Contenido del sitio</h1>
+    <a href="cerrar.php">Cerrar Sesion</a>
+    
+    <div class="contenido">
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quos repudiandae dignissimos facere repellendus odio labore aspernatur numquam reprehenderit, qui, culpa odit doloremque maiores nobis beatae impedit possimus nam cupiditate!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque optio quidem architecto dignissimos fugit, eius autem vitae deserunt, consequuntur nihil amet minus, distinctio voluptate fugiat ipsam similique, saepe quibusdam. Veniam.
+        </p>
+      </article>
+    </div>
+  </div>
+</body>
+</html>
