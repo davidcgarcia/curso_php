3b2539e286713cea8bec9e7467022e7676500280
davidcgarcia <davidg9404@gmail.com>
2016-07-05 11:55:21 -0500

    Obteniendo las Imagenes de la Base de Datos

--- practicas/galeria/index.php
+++ practicas/galeria/index.php
@@ -1,6 +1,36 @@
 <?php
 
+require 'funciones.php';
 
+
+$fotosPorPagina = 8;
+$paginaActual = isset($_GET['p']) ? (int)$_GET['p'] : 1;
+$inicio = ($paginaActual > 1) ? ($paginaActual * fotosPorPagina) - $fotosPorPagina : 1;
+
+$conexion = conexion('imagenes', 'root', 'L4r4v3l*.*2016');
+
+if (!$conexion) {
+  die();
+}
+
+$sql = "SELECT SQL_CALC_FOUND_ROWS * FROM fotos LIMIT $inicio, $fotosPorPagina";
+
+$statement = $conexion->prepare($sql);
+$statement->execute();
+$fotos = $statement->fetchAll();
+
+if (!$fotos) {
+  header('Location: index.php');
+}
+
+$sql = 'SELECT FOUND_ROWS() as total_filas';
+$statement = $conexion->prepare($sql);
+$statement->execute();
+$totalPost = $statement->fetch()['total_filas'];
+
+$totalPaginas = ceil($totalPost / $fotosPorPagina);
+
+echo $totalPaginas;
 require 'views/index.view.php';
 
 ?>
