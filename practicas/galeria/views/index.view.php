5bdaed36d554bc04308b90e377e5b118142ce395
davidcgarcia <davidg9404@gmail.com>
2016-07-05 09:29:17 -0500

    Creando la pagina de inicio

---
+++ practicas/galeria/views/index.view.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/estilos.css">
+  <title>Galería</title>
+</head>
+<body>
+  <header>
+    <div class="contenedor">
+      <h1 class="titulo">Mi Galería</h1>
+    </div>
+  </header>
+
+  <section class="fotos">
+    <div class="contenedor">
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/1.jpg">
+        </a>
+      </div>
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/2.jpg">
+        </a>
+      </div>
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/3.jpg">
+        </a>
+      </div>
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/4.jpg">
+        </a>
+      </div>
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/5.jpg">
+        </a>
+      </div>
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/6.jpg">
+        </a>
+      </div>
+      
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/7.jpg">
+        </a>
+      </div>
+
+      <div class="thumb">
+        <a href="#">
+          <img src="imagenes/8.jpg">
+        </a>
+      </div>
+
+      <div class="paginacion">
+        <a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i> Pagina Anterior </a>
+        <a href="#" class="derecha"> Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <p class="copyright">Galería creada por mí</p>
+  </footer>
+</body>
+</html>
