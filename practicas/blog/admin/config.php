519eb033ff77879cb19c2a177c8c47a155f31431
davidcgarcia <davidg9404@gmail.com>
2016-07-05 15:08:57 -0500

    Creando la pagina de inicio

---
+++ practicas/blog/admin/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('RUTA', 'http://localhost/curso_php/practicas/blog/');
+
+$database = [
+  'dbname'    => 'curso_blog', 
+  'user'      => 'root',
+  'password'  => 'L4r4v3l*.*2016'
+];
+
+$blogConfig = [
+  'post_por_pagina'   => '2', 
+  'carpeta_imagenes'  => 'imagenes/', 
+];
+
+$blogAdmin = [
+  'usuario'   => 'cristhian',
+  'password'  => '123'
+];
+
+?>
